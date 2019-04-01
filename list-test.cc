69b560486ddaecea8f91554c882cc65189e76e98
erira301 <47356581+erira301@users.noreply.github.com>
2019-04-01 13:41:18 +0200

    Create list-test.cc

---
+++ list-test.cc
@@ -0,0 +1,44 @@
+#include "List.h"
+
+#include "catch.hpp"
+
+TEST_CASE( "Create list" )
+{
+  
+  List lst{1,4,2,6,8,9};
+  
+  CHECK(lst.at(2) == 2);
+  CHECK(lst.size() == 6);
+  List l2;
+  l2 = lst;
+  CHECK(l2.size() == lst.size());
+  CHECK(l2.front() == lst.front());
+  CHECK(l2.back() == lst.back());
+
+  CHECK(l2.back() == 9);
+  CHECK(l2.size() == 6);
+  CHECK(l2.at(0) == 1);
+  
+  l2.push_back(6);
+  CHECK(l2.back() == 6);
+  l2.push_front(11);
+  CHECK(l2.front() == 11);
+  CHECK(l2.empty() == false);
+
+  List lst2{3,5,7};
+  l2 = std::move(lst2);
+  CHECK(l2.at(0) == 3);
+  CHECK(l2.size() == 3);
+  l2.at(0) = 15;
+  CHECK(l2.at(0) == 15);
+
+  l2.swap(lst);
+
+  CHECK(l2.at(0) == 1);
+  CHECK(lst.at(0) == 15);
+
+  
+  
+}
+
+
